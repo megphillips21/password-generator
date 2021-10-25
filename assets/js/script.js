bdb549e3335ecde55753df3295913295119c30a2
megphillips21 <megphillips21@gmail.com>
2021-10-24 20:00:22 -0700

    created variables for characters added if statements to check validity window confirms added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,38 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var allcaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lower = "abcdeghijklmopqrstuvwxyz"
+var characters= "!@#$%^&*()"
+var numbers = "1234567890"
+
+function generatePassword() {
+  console.log("you clicked the button");
+
+  // prompt user for password criteria
+  // length 8-128
+  var passwordLength = window.prompt("How many characters do you need the password to be?")
+  console.log(passwordLength)
+  if (passwordLength > 8 && passwordLength < 128) {
+    // include lowercase, uppercase, special, numbers
+    var uppercase = window.confirm("Would you like the password to include uppercase letters?")
+    var lowercase = window.confirm("Would you like the password to include lowercase letters?")
+    var numeric = window.confirm("Would you like the password to include numbers?")
+    var special = window.confirm("Would you like the password to include special characters letters?")
+    // validate the input- must have correct length and select at least one character type
+    if (uppercase || lowercase || numeric || special) {
+
+      // generate password 
+
+    } else {
+      window.alert("Must choose one option")
+    }
+
+  } else {
+    window.alert("Invalid password length")
+  }
+
+}
 
 // Write password to the #password input
 function writePassword() {
